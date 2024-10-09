e1b64cac45405d8ab4f068e7393f639c6859c851
Aryan <119997795+Aryan2556@users.noreply.github.com>
2024-10-09 22:59:50 +0530

    Files Updated..

---
+++ backend/models/index.js
@@ -0,0 +1,11 @@
+import Coupon from './coupon.model.js';
+import Order from './order.model.js';
+import Product from './product.model.js';
+import User from './user.model.js';
+
+export{
+    Coupon,
+    Order,
+    Product,
+    User,
+};
