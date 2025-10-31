04380132e0493ca127744952460eb52ef19e470f
Aryan <119997795+Aryan2556@users.noreply.github.com>
2025-10-31 11:09:21 +0530

    updated ....

--- frontend/src/lib/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const AxiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:7800/api" : "/api",
-    withCredentials: true,
-})
-
-
-export default AxiosInstance;
